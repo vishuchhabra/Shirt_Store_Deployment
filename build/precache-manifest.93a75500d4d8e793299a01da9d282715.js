7dede333125ac3d09397e5039ba5c71abd9bd9d4
vishuchhabra <vishuchhabra1016@gmail.com>
2020-12-10 20:07:16 +0530

    added payment gateway

---
+++ build/precache-manifest.93a75500d4d8e793299a01da9d282715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712bacaea6f93ff74152f225b92db93d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f696d97992e3702bd225",
+    "url": "/static/css/main.9e8a457f.chunk.css"
+  },
+  {
+    "revision": "56a8b46d059b30f97844",
+    "url": "/static/js/2.47b91cb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.47b91cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f696d97992e3702bd225",
+    "url": "/static/js/main.ded6d440.chunk.js"
+  },
+  {
+    "revision": "72017dee5443dfc32ffd",
+    "url": "/static/js/runtime-main.dc49cf19.js"
+  }
+]);
